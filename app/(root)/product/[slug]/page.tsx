ca45532b197f5eefeef98f1509b86c1a42c78eed
Nikhil Pandey <nikhil.pandey@annalect.com>
2025-03-05 04:45:16 +0530

    cart page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import Productimages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product.price";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.action";
 import { notFound } from "next/navigation";
+import AddToCart from "@/components/shared/product/add-to-cart";
+import { getMyCart } from "@/lib/actions/cart.action";
 
 const ProductDetailsPage = async(props: {
     params: Promise<{ slug: string }>;
@@ -14,6 +15,7 @@ const ProductDetailsPage = async(props: {
     if(!product) {
         return notFound();
     }
+    const cart = await getMyCart();
     return (
         <>
         <section>
@@ -57,7 +59,18 @@ const ProductDetailsPage = async(props: {
                             </div>
                             {product.stock > 0 && (
                   <div className='flex-center'>
-                    <Button className='w-full'>Add To Cart</Button>
+                    <AddToCart 
+                    cart={cart}
+                    item={
+                        {
+                            productId: product.id,
+                            name: product.name,
+                            slug: product.slug,
+                            price: product.price,
+                            qty: 1,
+                            image: product.images![0]
+                        }
+                    } />
                   </div>
                 )}
                         </CardContent>
--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { prisma } from '@/db/prisma';
 import { compareSync } from 'bcrypt-ts-edge';
 import type { NextAuthConfig } from 'next-auth';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export const config = {
     pages: {
@@ -54,13 +56,56 @@ export const config = {
     callbacks: {
         async session({session, user, trigger, token}: any) {
             session.user.id = token.sub;
+            session.user.role = token.role;
+            session.user.name = token.name;
+
             // If there is an update, set the user name
             if(trigger === 'update'){
                 session.user.name = user.name;
             }
             return session;
+        },
+        async jwt({ token, user, trigger, session }: any) {
+            // Assign user fields to token
+            if (user) {
+              token.id = user.id;
+              token.role = user.role;
+      
+              // If user has no name then use the email
+              if (user.name === 'NO_NAME') {
+                token.name = user.email!.split('@')[0];
+      
+                // Update database to reflect the token name
+                await prisma.user.update({
+                  where: { id: user.id },
+                  data: { name: token.name },
+                });
+              }
+            }
+            return token;
+        },
+        authorized({request, auth}: any) {
+            // Check if the user is authorized
+            if(!request.cookies.get('sessionCartId')) {
+                // generate new session cart id cookie
+                const sessionCartId = crypto.randomUUID();
+                //Clone request headers
+                const newRequestHeaders = new Headers(request.headers);
+                // Create new response headers
+                const response = NextResponse.next({
+                    request:{
+                        headers: newRequestHeaders
+                    }
+                })
+                // Set the session cart id cookie
+                response.cookies.set('sessionCartId', sessionCartId);
+                return response;
+            }else{
+                return true;
+            }
         }
     }
+  
 } satisfies NextAuthConfig;
 
 export const {handlers, auth, signIn, signOut} =  NextAuth(config);

3b88d19fafc32baddfeae3ad3a618e1113dacf85
Nikhil Pandey <nikhil.pandey@annalect.com>
2025-03-09 15:35:22 +0530

    remove import cn

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";

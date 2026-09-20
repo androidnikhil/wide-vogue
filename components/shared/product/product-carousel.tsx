1cf6236a3a2aaeeaefa0f3be5d8409cd3685bb95
Nikhil Pandey <nikhil.pandey@omc.com>
2026-09-20 08:24:24 +0530

    refactor: remove product name overlay from product carousel

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -40,11 +40,7 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
                   sizes='100vw'
                   className='w-full h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] object-cover object-center'
                 />
-                <div className='absolute inset-0 flex items-end justify-center'>
-                  <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
-                    {product.name}
-                  </h2>
-                </div>
+
               </div>
             </Link>
           </CarouselItem>
